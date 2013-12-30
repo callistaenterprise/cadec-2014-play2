bc65db5efb422ed5df2b3e5b87e289bfac0479fe
Anders Asplund <aasplund@gmail.com>
2013-12-30 08:41:05 +0100

    Serve json from service

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,9 +3,10 @@ package controllers
 import play.api._
 import play.api.mvc._
 import play.api.libs.concurrent.Execution.Implicits._
-import play.api.libs.ws.{Response, WS}
+import play.api.libs.ws.{WS}
 import scala.concurrent.Future
 import play.api.libs.json._
+import play.api.libs.json.Json._
 import play.api.libs.functional.syntax._
 import play.api.data.Forms._
 import views._
@@ -31,36 +32,44 @@ object Application extends Controller {
   val getConfig = (key: String) => config.getString(key).get
 
   implicit val locationReads: Reads[Location] = (
-    (JsPath \ "geometry" \ "location" \ "lng").read[Double] and
-      (JsPath \ "geometry" \ "location" \ "lat").read[Double] and
-      (JsPath \ "formatted_address").read[String]
-    )(Location)
+    (JsPath \ "geometry" \ "location" \ "lng").read[Double].map(_.toString.substring(0,5)) and
+    (JsPath \ "geometry" \ "location" \ "lat").read[Double].map(_.toString.substring(0,5)) and
+    (JsPath \ "formatted_address").read[String]
+  )(Location)
 
   implicit val weatherReads: Reads[Weather] = (
     (JsPath \\ "validTime").read[String].map(DateTime.parse(_, dateTimeNoMillis)) and
-      (JsPath \\ "t").read[Double].map(_.toString)
-    )(Weather)
+    (JsPath \\ "t").read[Double].map(_.toString)
+  )(Weather)
+
+  implicit val localWeatherWrites: Writes[models.LocalWeather] = (
+    (JsPath \ "lng").write[String] and
+    (JsPath \ "lat").write[String] and
+    (JsPath \ "temp").lazyWrite(Writes.mapWrites[String])
+  )(unlift(models.LocalWeather.unapply))
 
 
   def getLocations(address: String): Future[Seq[Location]] = {
     val response = WS.url(getConfig("maps.api")).withQueryString("address" -> address, "sensor" -> "false")
       .get
+      .filter (_.status == OK)
+
     response map (r => (r.json \ "results").as[Seq[Location]])
   }
 
-  def getWeather(locations: Seq[Location]): Future[Seq[LocalWeather]] = {
+  def getWeatherFromSmhi(locations: Seq[Location]): Future[Seq[LocalWeather]] = {
     Future.sequence(
       locations.map {
         location =>
-          val response = WS.url(getConfig("smhi.url").format(location.lat.toString.substring(0, 5), location.lng.toString.substring(0, 5))).get
+          val response = WS.url(getConfig("smhi.url").format(location.lat, location.lng)).get
           response map (r => loadCurrentTempFromForecasts(r.json, location))
       }
     )
   }
 
   private def loadCurrentTempFromForecasts(json: JsValue, l: Location): LocalWeather = {
-    val w =(json \ "timeseries").as[Seq[Weather]].filter(t => t.time.isAfterNow())(0)
-    l.withWeather(w)
+    val w = (json \ "timeseries").as[Seq[Weather]].filter(t => t.time.isAfterNow())(0)
+    l.withWeather("smhi", w)
   }
 
 
@@ -71,8 +80,8 @@ object Application extends Controller {
       },
         address => {
           getLocations(address.name) flatMap {
-            getWeather(_)
-          } map (s => Ok(html.temp(s)))
+            getWeatherFromSmhi(_)
+          } map (s => Ok(toJson(s)))
         })
   }
 
--- app/models/Models.scala
+++ app/models/Models.scala
@@ -4,10 +4,11 @@ import org.joda.time.DateTime
 
 case class Address(name: String)
 
-case class Location(lng: Double, lat: Double, address: String) {
-  def withWeather(weather: Weather) = LocalWeather(this, weather)
+case class Location(lng: String, lat: String, address: String) {
+  def withWeather(weatherService: String, weather: Weather) = LocalWeather(lng, lat, Map(weatherService -> weather.temp))
 }
 
-case class LocalWeather(location: Location, weather: Weather)
+case class LocalWeather(lng: String, lat: String, temp:Map[String, String])
 
 case class Weather(time: DateTime, temp: String)
+
