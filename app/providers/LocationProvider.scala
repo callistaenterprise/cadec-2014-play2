df236c83570481379ac3eb731a9d52c6f334da04
Par Wenaker <par.wenaker@callistaenterprise.se>
2014-01-10 16:28:03 +0100

    Extracted method from LocationProvider

--- app/providers/LocationProvider.scala
+++ app/providers/LocationProvider.scala
@@ -3,7 +3,7 @@ package providers
 import models.JsonHelper._
 import models.Location
 import play.api.libs.concurrent.Execution.Implicits._
-import play.api.libs.ws.WS
+import play.api.libs.ws.{Response, WS}
 import scala.concurrent.Future
 
 
@@ -12,7 +12,8 @@ trait LocationProvider {
     WS.url(config("maps.api"))
       .withQueryString("address" -> address, "sensor" -> "false")
       .get()
-      .map(r => (r.json \ "results").as[Seq[Location]])
+      .map(response => responseToLocations(response))
   }
 
+  private def responseToLocations(response: Response): Seq[Location] = (response.json \ "results").as[Seq[Location]]
 }
